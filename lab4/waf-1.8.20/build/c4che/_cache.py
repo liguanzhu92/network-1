f42828ac2f2de9dada73b0d5e605f1949843cec7
Jiabei Xu <xjbhenry@gmail.com>
2016-03-21 23:13:23 -0400

    added ns3

---
+++ lab4/waf-1.8.20/build/c4che/_cache.py
@@ -0,0 +1,8 @@
+BINDIR = '/usr/local/bin'
+LIBDIR = '/usr/local/lib64'
+PREFIX = '/usr/local'
+PYC = 1
+PYFLAGS = ''
+PYFLAGS_OPT = '-O'
+PYO = 1
+PYTHON = ['/usr/bin/python']
---
+++ lab4/waf-1.8.20/build/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x1081400
+tools = [{'tool': 'python', 'tooldir': None, 'funs': None}]
