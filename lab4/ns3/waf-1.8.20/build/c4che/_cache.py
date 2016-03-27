40523a47dfdd8816ad877c28e227b185a0978c5e
Jiabei Xu <xjbhenry@gmail.com>
2016-03-27 12:01:34 -0400

    comp

--- lab4/ns3/waf-1.8.20/build/c4che/_cache.py
+++
@@ -1,8 +0,0 @@
-BINDIR = '/usr/local/bin'
-LIBDIR = '/usr/local/lib64'
-PREFIX = '/usr/local'
-PYC = 1
-PYFLAGS = ''
-PYFLAGS_OPT = '-O'
-PYO = 1
-PYTHON = ['/usr/bin/python']
--- lab4/waf-1.8.20/build/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x1081400
-tools = [{'tool': 'python', 'tooldir': None, 'funs': None}]
