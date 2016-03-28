4f44a70028be8d8227954a4a9b02d0632b4f122c
Jiabei Xu <xjbhenry@gmail.com>
2016-03-28 16:22:31 -0400

    add verbose argument

--- lab4/ns-3.24.1/scratch/udpchain.cc
+++ lab4/ns-3.24.1/scratch/udpchain.cc
@@ -1,70 +1,93 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("UDPChain");
+NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 
 int
 main (int argc, char *argv[])
 {
-	bool verbose = true;
-	CommandLine cmd;
-	cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
-	cmd.Parse (argc,argv);
-	if (verbose)
-	{
-		LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-		LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-	}
+  int enableVerbose = 0;
+  if(argc == 2) {
+    if(strcmp(argv[1], "-v") == 0) {
+      enableVerbose = 1;
+    }
+  }
+  Time::SetResolution (Time::NS);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+  if(enableVerbose) {
+    LogComponentEnableAll(LOG_LEVEL_INFO);
+  }
+  
+  //p2p nodes A---B
+  NodeContainer nodesAB;
+  nodesAB.Create (2);
 
-	NodeContainer nodes;
-	nodes.Create (4);
+  //p2p nodes B--C
+  NodeContainer nodesBC;
+  nodesBC.Add (nodesAB.Get (1)); 
+  nodesBC.Create (1);
 
-	CsmaHelper csma;
-	csma.SetChannelAttribute ("DataRate", StringValue ("1Mbps"));
-	csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (10)));
+  //p2p nodes C--D
+  NodeContainer nodesCD;
+  nodesCD.Add (nodesBC.Get (1));
+  nodesCD.Create(1);
 
-	NetDeviceContainer devices;
-	devices = csma.Install (nodes);
 
-	InternetStackHelper stack;
-	stack.Install (nodes);
+  //create channel
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("1Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("10ms"));
+  
+  NetDeviceContainer devicesAB;
+  devicesAB = pointToPoint.Install (nodesAB);
+  NetDeviceContainer devicesBC;
+  devicesBC = pointToPoint.Install (nodesBC);
+  NetDeviceContainer devicesCD;
+  devicesCD = pointToPoint.Install (nodesCD);
 
-	Ipv4AddressHelper address;
-	address.SetBase ("10.1.1.0", "255.255.255.0");
+  pointToPoint.EnablePcapAll ("udp");
 
-	Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  //install stack and assign IP
+  InternetStackHelper stack;
+  stack.Install (nodesAB);
+  stack.Install (nodesBC.Get (1));
+  stack.Install (nodesCD.Get (1));
 
-	UdpEchoServerHelper echoServer (9);
+  Ipv4AddressHelper addressAB;
+  addressAB.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4AddressHelper addressBC;
+  addressBC.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4AddressHelper addressCD;
+  addressCD.SetBase ("10.1.3.0", "255.255.255.0");
 
-	ApplicationContainer serverApps = echoServer.Install (nodes.Get (3));
-	serverApps.Start (Seconds (1.0));
-	serverApps.Stop (Seconds (10.0));
+  Ipv4InterfaceContainer interfacesAB = addressAB.Assign (devicesAB);
+  Ipv4InterfaceContainer interfacesBC = addressBC.Assign (devicesBC);
+  Ipv4InterfaceContainer interfacesCD = addressCD.Assign (devicesCD);
 
-	UdpEchoClientHelper echoClient (interfaces.GetAddress (3), 9);
-	echoClient.SetAttribute ("MaxPackets", UintegerValue (5));
-	echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  	echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  //echo server
+  UdpEchoServerHelper echoServer (9);
+  ApplicationContainer serverApps = echoServer.Install (nodesCD.Get (1));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
 
-  	ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
-  	clientApps.Start (Seconds (2.0));
-  	clientApps.Stop (Seconds (10.0));
-
-  	Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-	csma.EnablePcap ("second", devices.Get (0), true);
-	csma.EnablePcap ("second", devices.Get (1), true);
-	csma.EnablePcap ("second", devices.Get (2), true);
-	csma.EnablePcap ("second", devices.Get (3), true);
-
-  	Simulator::Run ();
-  	Simulator::Destroy ();
+  //echo client
+  UdpEchoClientHelper echoClient (interfacesCD.GetAddress (1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (5));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  ApplicationContainer clientApps = echoClient.Install (nodesAB.Get (0));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+  
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  Simulator::Run ();
+  Simulator::Destroy ();
   return 0;
 }
